91b89950e5a1f432ea7d3890b6ae106daa05af64
sabersword <ameyag416@gmail.com>
2017-09-29 18:42:08 +0530

    ADD: DTree training and saving functions

--- augmented_dtree.py
+++ augmented_dtree.py
@@ -1,12 +1,81 @@
+import os
+from augmented_dtree_node import DTNode
+from classifiers.classifier import Classifier
+from classifiers.perceptron import Perceptron
+
 class DTree(object):
 	"""DTree class to store tree structure"""
-	def __init__(self, num_classes, num_child, max_nodes, max_depth, data_type):
+	
+	def __init__(self, num_classes, num_child, max_depth, data_type, data_dimension):
+		"""
+		Arguments:
+		num_classes: Number of classes in data
+		num_child:	Numer of child nodes per decision node
+		max_depth:	Maximum depth of decision tree
+		data_type:	One of {'numeric','image'}
+		data_dimension:	Number of features in data; int for numeric , tuple of ints for image
+		"""
 		super(DTree, self).__init__()
 		self.num_classes = num_classes
 		self.num_child = num_child
-		self.max_nodes = max_nodes
 		self.max_depth = max_depth
 		self.data_type = data_type
 		self.data_dimension = data_dimension
+		if self.data_type == 'numeric' and self.num_classes>2:
+			self.decision_type = Perceptron
+		else:
+			raise NotImplementedError('Feature not implemented')
 		self.nodes = []
 		self.built = False
+
+	def train(self, data_file, epochs_per_node, batch_size, model_save_path):
+		"""
+		Build tree and save node parameters
+		Arguments:
+		data_file:	Data file for root node. Other nodes create their own file
+		epochs_per_node: Number of training epochs for each node
+		batch_size:	Batch size for training and predictions
+		model_save_path:	Directory to save node parameters
+		"""
+		base = os.path.split(data_file)[0]
+		node_to_process = 0
+		new_node = DTNode(node_id=0, parent_id=0, node_depth=0, num_classes=self.num_classes,
+		 num_child=self.num_child, data_file=data_file)
+		self.nodes.append(new_node)
+		while True:
+			try:
+				curr_node = self.nodes[node_to_process]
+			except IndexError:
+				print "{} nodes processed. Tree building done.".format(len(self.nodes))
+				break
+			curr_node.set_decision_maker(self.decision_type(input_dim=self.data_dimension, output_dim=self.num_child,
+				 num_classes=self.num_classes, epochs=epochs_per_node, batch_size=batch_size))
+			curr_node.set_child_id_start(len(self.nodes))
+			child_list = curr_node.train()
+			curr_node.save_node_params(model_save_path)
+
+			if child_list == [-1]:
+				continue
+			
+			for i in child_list:
+				# stop tree growth at max_depth
+				num_child = self.num_child if self.max_depth>1+curr_node.node_depth else 0
+				new_node = DTNode(node_id=i, parent_id=curr_node.node_id, node_depth=1+curr_node.node_depth,
+				 num_classes=self.num_classes, num_child=num_child,
+				  data_file=os.path.join(base,'data_{}.csv'.format(i)) )
+				self.nodes.append(new_node)
+			
+			node_to_process += 1
+		self.built = True
+
+	def save(self, model_save_file):
+		"""
+		Save tree structure. NOTE: Node parameters are saved separately
+		Arguments:
+		model_save_file:	File to save tree structure
+		"""
+		structure = {}
+		for i in self.nodes:
+			structure[i.node_id] = i.child_id
+		with open(os.path.join(model_save_file), 'wb') as savefile:
+			pickle.dump(structure, savefile, protocol=pickle.HIGHEST_PROTOCOL)

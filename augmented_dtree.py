b838d46aa268927593126e08ab0f372a1c982680
Spoorthy Bhat <spoorthy.bhat.77@gmail.com>
2017-10-04 21:23:17 +0530

    ADD: Load tree from model

--- augmented_dtree.py
+++ augmented_dtree.py
@@ -2,11 +2,13 @@ import os
 from augmented_dtree_node import DTNode
 from classifiers.classifier import Classifier
 from classifiers.perceptron import Perceptron
+from classifiers.C45 import C45
+from dataBalancing import DataBalance
 
 class DTree(object):
 	"""DTree class to store tree structure"""
 	
-	def __init__(self, num_classes, num_child, max_depth, data_type, data_dimension, data_balance):
+	def __init__(self, num_classes, num_child, max_depth, data_type, data_dimension, data_balance, decision_type):
 		"""
 		Arguments:
 		num_classes: Number of classes in data
@@ -15,6 +17,7 @@ class DTree(object):
 		data_type:	One of {'numeric','image'}
 		data_dimension:	Number of features in data; int for numeric , tuple of ints for image
 		data_balance: Bool (whether to use data_balancing)
+		decision_type: Classifier to be used
 		"""
 		super(DTree, self).__init__()
 		self.num_classes = num_classes
@@ -23,12 +26,16 @@ class DTree(object):
 		self.data_type = data_type
 		self.data_dimension = data_dimension
 		self.data_balance = data_balance
-		if self.data_type == 'numeric' and self.num_classes>2:
-			self.decision_type = Perceptron
-		else:
-			raise NotImplementedError('Feature not implemented')
+		self.decision_type = decision_type
 		self.nodes = []
 		self.built = False
+		
+		#if self.data_type == 'numeric' and self.num_classes>2:
+		#	self.decision_type = Perceptron
+		#else:
+		#	raise NotImplementedError('Feature not implemented')
+		
+		
 
 	def train(self, data_file, epochs_per_node, batch_size, model_save_path):
 		"""
@@ -54,9 +61,10 @@ class DTree(object):
 				 num_classes=self.num_classes, epochs=epochs_per_node, batch_size=batch_size))
 			curr_node.set_child_id_start(len(self.nodes))
 			if self.data_balance:
-				# FIX
+				db = DataBalance(os.path.join(base,'data_{}.csv'.format(i)) )
+				db.data_balance(os.path.join(base,'b_data_{}.csv'.format(i)))
 				# databalance(input=os.path.join(base,'data_{}.csv'.format(i)), output=os.path.join(base,'b_data_{}.csv'.format(i)))
-				pass
+				
 			child_list = curr_node.train()
 			curr_node.save_node_params(model_save_path)
 
@@ -73,7 +81,7 @@ class DTree(object):
 				 num_classes=self.num_classes, num_child=num_child,
 				  data_file=os.path.join(base,'data_{}.csv'.format(i)), balanced_file=os.path.join(base,balance_filename) )
 				
-				self.nodes.append(new_node)
+				self.nodes.append(new_node)				
 			
 			node_to_process += 1
 		self.built = True
@@ -89,3 +97,42 @@ class DTree(object):
 			structure[i.node_id] = i.child_id
 		with open(os.path.join(model_save_file), 'wb') as savefile:
 			pickle.dump(structure, savefile, protocol=pickle.HIGHEST_PROTOCOL)
+
+	def load_tree(self, model_save_file, model_save_path):
+		structure = {}
+		with open(os.path.join(model_file), 'rb') as modelfile:
+			structure = pickle.load(f)
+
+		node_to_process = 0
+		new_node = DTNode(node_id=0, parent_id=0, node_depth=0, num_classes=self.num_classes,
+		 num_child=self.num_child)
+		self.nodes.append(new_node)
+		while True:
+			try:
+				curr_node = self.nodes[node_to_process]
+			except IndexError:
+				print "{} nodes processed. Tree building done.".format(len(self.nodes))
+				break
+			curr_node.set_decision_maker(self.decision_type(input_dim=self.data_dimension, output_dim=self.num_child,
+				 num_classes=self.num_classes, epochs=epochs_per_node, batch_size=batch_size))
+			curr_node.load_node_params(model_save_path)
+			
+			child_list = structure[curr_node.node_id]
+
+			if child_list == [-1]:
+				continue
+			
+			for i in child_list:
+				# stop tree growth at max_depth
+				num_child = self.num_child if self.max_depth>1+curr_node.node_depth else 0
+				# if not using data balancing, send original file as balanced file
+				
+				new_node = DTNode(node_id=i, parent_id=curr_node.node_id, node_depth=1+curr_node.node_depth,
+				 num_classes=self.num_classes, num_child=num_child)			
+				self.nodes.append(new_node)				
+			
+			node_to_process += 1
+
+	def predict(self, model_file, model_save_path, data_file):
+		self.load_tree(model_file, model_save_path)
+		df = pd.read_csv(self.data_file, names=['features'])

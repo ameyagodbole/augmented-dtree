FIX: Impurity calculation and use of impurity drop threshold

augmented_dtree.py
@@ -119,11 +119,12 @@ class DTree(object):
 				continue
 
 			if self.impurity_drop_threshold is not None:
-				if self.get_impurity_drop(self.nodes[curr_node.parent_id], curr_node) < self.impurity_drop_threshold :
+				if self.get_impurity_drop(curr_node) < self.impurity_drop_threshold :
 					curr_node.child_id = []
 					curr_node.num_child = 0
 					curr_node.is_decision_node = True
 					curr_node.label = curr_node.get_label()
+					curr_node.label_type = 'impurity_threshold'
 					logging.debug('Stop growth at node {} due to low impurity drop rate'.format(node_to_process))
 					node_to_process += 1
 					continue
@@ -161,7 +162,8 @@ class DTree(object):
 			node_info['is_decision_node'] = i.is_decision_node
 			node_info['label'] = i.label
 			node_info['label_type'] = i.label_type
-			node_info['impurity'] = i.get_impurity()
+			node_info['impurity'] = i.get_self_impurity()
+			node_info['split_impurity'] = i.get_split_impurity()
 			structure[i.node_id] = node_info
 		logging.debug('Saving to {}'.format(model_save_file))
 		with open(model_save_file, 'wb') as savefile:
@@ -210,9 +212,10 @@ class DTree(object):
 		self.load_tree(model_save_file, model_save_path)
 		df = pd.read_csv(data_file, index_col='assigned_node')
 		df['predicted_label'] = [0 for _ in range(len(df))]
+		df['label_depth'] = [0 for _ in range(len(df))]
 		for node in self.nodes:
 			node.predict(df)
-		df = df[['label','predicted_label']]
+		df = df[['label','predicted_label','label_depth']]
 		df.to_csv(os.path.join(working_dir_path, output_file),index=False)
 		acc = pd.np.sum(df['label']==df['predicted_label']).astype(pd.np.float32)  / len(df)
 		logging.info("Accuracy: {}".format(acc))
@@ -223,13 +226,10 @@ class DTree(object):
 		f.write("Number of nodes: {}".format(len(self.nodes)))
 		f.close()
 
-	def get_impurity_drop(self, parent_node, child_node):
+	def get_impurity_drop(self, node):
 		"""
-		Find impurity drop from parent to child node.
+		Find impurity drop from current node to child nodes
 		Arguments:
-		parent_node: Parent node
-		child_node: Child node	
+		node : Node to process for impurity threshold criterion
 		"""
-		if child_node.node_id == 0:
-			return float('inf')
-		return (parent_node.get_impurity() - child_node.get_impurity())
+		return (node.get_self_impurity() - node.get_split_impurity())

classifiers/C45.py
@@ -27,6 +27,7 @@ class C45(Classifier):
 		self.num_classes = num_classes
 		self.index = None
 		self.value = None
+		self.impurity = None
 		self.score = None
 		self.impurity_drop = None
 		self.b_score = None
@@ -121,6 +122,8 @@ class C45(Classifier):
 			logging.debug('Decide label node based on count_threshold')
 			return True, 'count_threshold'
 		counts = Counter(df['label'])
+		class_prob = np.array(counts.values()).astype(np.float32)/len(df)
+		self.impurity = -np.sum(class_prob*np.log2(class_prob))
 		if np.float(counts.most_common(1)[0][1])/len(df) > purity_threshold:
 			if len(counts)<=1:
 				# Only class in data "purity_threshold I"
@@ -143,7 +146,10 @@ class C45(Classifier):
 		counts = np.asarray([len(df[df['label']==c]) for c in range(self.num_classes)])
 		return np.argmax(counts).astype(np.int32)
 
-	def get_impurity(self):
+	def get_self_impurity(self):
+		return self.impurity
+
+	def get_split_impurity(self):
 		return self.score
 
 	def predict(self, node_id, params, df, child_id):

classifiers/perceptron.py
@@ -29,7 +29,8 @@ class Perceptron(Classifier):
 		self.node_id = node_id
 		self.built = False
 		self.graph = None
-		self.score = 0.0
+		self.impurity = None
+		self.score = None
 
 	def build(self):
 		"""
@@ -126,7 +127,7 @@ class Perceptron(Classifier):
 								continue
 							if curr_loss_drop < 0 or curr_loss_drop < 0.05*max_loss_drop:
 								patience += 1
-							if patience>=6:
+							if patience>=2:
 								logging.info("Stopping training after {} epochs due to saturation of perceptron".format(e+1))
 								break
 				if not all_ok:
@@ -159,7 +160,7 @@ class Perceptron(Classifier):
 		"""
 		if len(df)==0:
 			return
-		x = df.loc[node_id, (df.columns!='predicted_label') & (df.columns!='label')].as_matrix()
+		x = df.loc[node_id, (df.columns!='predicted_label') & (df.columns!='label') & (df.columns!='label_depth')].as_matrix()
 		Wx = x.dot(params['W'])
 		Wxb = Wx + params['b']
 		preds = np.argmax(Wxb, 1)
@@ -185,6 +186,8 @@ class Perceptron(Classifier):
 			return True, 'count_threshold'
 		# counts = np.asarray([len(df[df['label']==c]) for c in range(self.num_classes)]).astype(np.float32)
 		counts = Counter(df['label'])
+		class_prob = np.array(counts.values()).astype(np.float32)/len(df)
+		self.impurity = -np.sum(class_prob*np.log2(class_prob))
 		if np.float(counts.most_common(1)[0][1])/len(df) > purity_threshold:
 			if len(counts) <= 1:
 				# Only class in data "purity_threshold I"
@@ -256,5 +259,8 @@ class Perceptron(Classifier):
 			df.to_csv(os.path.join(data_path, 'data', 'data_'+str(child_id[j])+'.csv'),index=False)
 		logging.debug('Node impurity = {}'.format(self.score))
 
-	def get_impurity(self):
+	def get_self_impurity(self):
+		return self.impurity
+
+	def get_split_impurity(self):
 		return self.score
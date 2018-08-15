6a73d0c6d3e3520c3dd732b5bda0bcaa386de8c2
ameyagodbole <ameyag416@gmail.com>
2018-08-15 18:11:22 +0530

    FIX: Update syntax in tests

--- test/test_perceptron_iris.py
+++ test/test_perceptron_iris.py
@@ -4,18 +4,19 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from augmented_dtree import DTree
 
 def main():
-	tree = DTree(num_classes = 3, num_child = 2, max_depth = 5, data_type = 'float', 
-		data_dimension = 4, data_balance = True, decision_type = 'Perceptron', count_threshold = 5,
-		 purity_threshold = 0.7, impurity_drop_threshold = 0.1)
-	tree.train(data_file = './output/iris_train.csv', working_dir_path='./output/perceptron_iris/', epochs_per_node = 200, batch_size = 20, model_save_path = None)
+	tree = DTree(num_classes = 3, num_child = 2, max_depth = 5, data_type = 'float', data_dimension = 4,
+	 data_balance = True, balance_mode='kmeans_os_no_us', decision_type = 'Perceptron', decision_criterion='entropy',
+	 purity_threshold = 0.7, count_threshold = 5, impurity_drop_threshold = 0.1)
+	tree.train(data_file = './output/iris_train.csv', working_dir_path='./output/perceptron_iris/',
+	 epochs_per_node = 200, batch_size = 20, model_save_path = None)
 	tree.save(path.join('./output/perceptron_iris/', 'test_perceptron_iris.pkl'))
 
-	tree_test = DTree(num_classes = 3, num_child = 2, max_depth = 5, data_type = 'float', 
-		data_dimension = 4, data_balance = True, decision_type = 'Perceptron', count_threshold = 5,
-		 purity_threshold = 0.7, impurity_drop_threshold = 0.1)
+	tree_test = DTree(num_classes = 3, num_child = 2, max_depth = 5, data_type = 'float', data_dimension = 4,
+	 data_balance = True, balance_mode='kmeans_os_no_us', decision_type = 'Perceptron', decision_criterion='entropy',
+	 purity_threshold = 0.7, count_threshold = 5, impurity_drop_threshold = 0.1)
 	tree_test.predict(model_save_file = path.join('./output/perceptron_iris/', 'test_perceptron_iris.pkl'), 
-		model_save_path = path.join('./output/perceptron_iris/', 'model'), data_file= './output/iris_test.csv', working_dir_path = './output/perceptron_iris/',
-		 output_file = 'perceptron_predict.csv')
+	 model_save_path = path.join('./output/perceptron_iris/', 'model'), data_file= './output/iris_test.csv',
+	 working_dir_path = './output/perceptron_iris/', output_file = 'perceptron_predict.csv')
 
 if __name__ == "__main__":
     main()
--- test/test_perceptron_mnist.py
+++ test/test_perceptron_mnist.py
@@ -4,17 +4,19 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from augmented_dtree import DTree
 
 def main():
-	tree = DTree(num_classes = 10, num_child = 2, max_depth = 20, data_type = 'float', 
-		data_dimension = 784, data_balance = False, decision_type = 'Perceptron', count_threshold = 10,
-		 purity_threshold = 0.6, impurity_drop_threshold = 0.1)
-	tree.train(data_file = './output/perceptron_mnist/mnist_train1.csv', epochs_per_node = 50, batch_size = 200, model_save_path = './output/perceptron_mnist/model/')
-	tree.save('./output/perceptron_mnist/tree_model')
+	tree = DTree(num_classes = 3, num_child = 2, max_depth = 5, data_type = 'float', data_dimension = 4,
+	 data_balance = True, balance_mode='hac_os_no_us', decision_type = 'Perceptron', decision_criterion='gini',
+	 purity_threshold = 0.7, count_threshold = 5, impurity_drop_threshold = 0.1)
+	tree.train(data_file = './output/perceptron_mnist/mnist_train1.csv',  working_dir_path='./output/perceptron_mnist/',
+	 epochs_per_node = 200, batch_size = 20, model_save_path = None)
+	tree.save(path.join('./output/perceptron_mnist/', 'test_perceptron_mnist.pkl'))
 
-	tree_test = DTree(num_classes = 10, num_child = 2, max_depth = 20, data_type = 'float', 
-		data_dimension = 784, data_balance = False, decision_type = 'Perceptron', count_threshold = 10,
-		 purity_threshold = 0.6, impurity_drop_threshold = 0.1)
-	tree_test.predict(model_save_file='./output/perceptron_mnist/tree_model',
-	 model_save_path = './output/perceptron_mnist//model/', data_file= './output/perceptron_mnist/mnist_test1.csv')
+	tree_test = DTree(num_classes = 3, num_child = 2, max_depth = 5, data_type = 'float', data_dimension = 4,
+	 data_balance = True, balance_mode='hac_os_no_us', decision_type = 'Perceptron', decision_criterion='gini',
+	 purity_threshold = 0.7, count_threshold = 5, impurity_drop_threshold = 0.1)
+	tree_test.predict(model_save_file = path.join('./output/perceptron_mnist/', 'test_perceptron_mnist.pkl'), 
+	 model_save_path = path.join('./output/perceptron_mnist/', 'model'), data_file= './output/mnist_test.csv',
+	 working_dir_path = './output/perceptron_mnist/', output_file = 'perceptron_predict.csv')
 
 
 if __name__ == "__main__":
